54b64c546ba510dbedf529cd954f847eb75a8100
PeterDevelops <PeterDinhBC@gmail.com>
2023-12-13 19:10:01 -0800

    Created an event click event listener

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 let addToDoButton = document.getElementById("addToDo");
 let toDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
+
+addToDoButton.addEventListener("click", function()){
+  let paragraph = document.createElement("p")
+  toDoContainer.appendChild(paragraph);
+};
