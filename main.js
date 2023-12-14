b1de99e45a7701cca56d24fbfc96749d961c0be5
PeterDevelops <PeterDinhBC@gmail.com>
2023-12-13 19:29:31 -0800

    Added a event listener function 'line-through'

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ let inputField = document.getElementById("inputField");
 
 addToDoButton.addEventListener("click", function(){
   let paragraph = document.createElement("p")
+  paragraph.classList.add("paragraph-styling");
   paragraph.innerText = inputField.value;
   toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener("click", function(){
+    paragraph.style.textDecoration = "line-through";
+  })
 })
